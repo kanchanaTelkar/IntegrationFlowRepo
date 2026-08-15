05da3c20808ab04263d96dfd2b4e03cc8097db1c
kanchanaTelkar <86785125+kanchanaTelkar@users.noreply.github.com>
2026-08-16 01:00:55 +0530

    t

--- MyIflowInGit/src/main/resources/script/script2.groovy
+++ MyIflowInGit/src/main/resources/script/script2.groovy
@@ -10,7 +10,7 @@ import com.sap.gateway.ip.core.customdev.util.Message;
 def Message processData(Message message) {
 
     message.setBody(body + " Body is modified in BASE");
-    message.setBody(body + " Body2 set in BASE");
+    message.setBody(body + " Body2 set in LOCAL");
     //To set or modify the body, you can use the following methods.
     def body = message.getBody();
     message.setBody(body + " Body is modified");
@@ -18,14 +18,17 @@ def Message processData(Message message) {
     //To set or modify the headers, you can use the following methods.
     def headers = message.getHeaders();
     def value = headers.get("oldHeader");
-    message.setHeader("oldHeaderBASE", value + " modified");
-    message.setHeader("newHeaderBASE", "newHeaderBASE");
+    message.setHeader("oldHeaderLOCAL", value + " modified");
+    message.setHeader("newHeaderLOCAL", "newHeaderBASE");
 
     message.setHeader("BASECODE2", value + " modified");
     message.setHeader("BASECODE2", "newHeader");
     
-    message.setHeader("oldHeaderBASE2", value + " modified");
-    message.setHeader("newHeaderBASE2", "newHeader");
+    message.setHeader("oldHeaderLOCAL2", value + " modified");
+    message.setHeader("newHeaderLOCAL2", "newHeader");
+    
+    message.setHeader("newInLOCAL", value + " modified");
+    message.setHeader("newInLOCAL2", "newHeader");
  
     return message;
 }
